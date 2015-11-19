773dace3e1ef54725ad13ab269cbd4d55007cf0b
tuff studio <chris@tuff-studio.com>
2015-11-19 15:36:48 +0000

    qode_carousel_tuff shortcode

--- functions.php
+++ functions.php
@@ -1214,3 +1214,165 @@ if (!function_exists('social_share_tuff')) {
 add_shortcode('social_share_tuff', 'social_share_tuff');
 
 
+/* Qode Carousel shortcode */
+
+if (!function_exists('qode_carousel_tuff')) {
+    function qode_carousel_tuff( $atts, $content = null ) {
+        $args = array(
+            "carousel" => "",
+            "orderby"  => "date",
+            "order"    => "ASC",
+            "show_in_two_rows" => ""
+        );
+        extract(shortcode_atts($args, $atts));
+
+        $html = "";
+        $carousel_holder_classes = "";
+        if ($carousel != "") {
+
+            if($show_in_two_rows == 'yes') {
+                $carousel_holder_classes = ' two_rows';
+            }
+
+            $html .= "<div class='qode_carousels_holder clearfix" . $carousel_holder_classes  ."'><div class='qode_carousels'><ul class='slides'>";
+
+            // $q = array('post_type'=> 'carousels', 'carousels_category' => $carousel, 'orderby' => $orderby, 'order' => $order, 'posts_per_page' => '-1');
+
+            $q = array(
+                    'post_type' => 'post',
+                    'category_name' => '',
+                    'orderby' => $orderby,
+                    'order' => $order,
+                    'posts_per_page' => '-1'
+                );
+
+            query_posts($q);
+
+            if ( have_posts() ) : $postCount = 1; while ( have_posts() ) : the_post();
+
+                // echo '<script>alert("'.$errorMessage.'");</script>';
+
+                // echo(get_post_meta(get_the_ID(),"qode_carousel-image",true));
+
+                // if(get_post_meta(get_the_ID(), "qode_carousel-image", true) != ""){
+                //     $image = get_post_meta(get_the_ID(), "qode_carousel-image", true);
+                // } else {
+                //     $image = "";
+                // }
+                
+                // $image = get_the_post_thumbnail(array(250,250));
+
+                $thumb_id = get_post_thumbnail_id();
+                $thumb_url = wp_get_attachment_image_src($thumb_id, array(100,100) , true);
+                $image = $thumb_url[0];
+
+
+                // $image = get_post_meta(get_the_ID(), "qode_carousel-image", true);
+
+                // if(get_post_meta(get_the_ID(), "qode_carousel-hover-image", true) != ""){
+                //     $hover_image = get_post_meta(get_the_ID(), "qode_carousel-hover-image", true);
+                //     $has_hover_image = "has_hover_image";
+                // } else {
+                //     $hover_image = "";
+                //     $has_hover_image = "";
+                // }
+
+                $hover_image = "yes";
+                $has_hover_image = "has_hover_image";
+
+                // if(get_post_meta(get_the_ID(), "qode_carousel-item-link", true) != ""){
+                //     $link = get_post_meta(get_the_ID(), "qode_carousel-item-link", true);
+                // } else {
+                //     $link = "";
+                // }
+
+                $link = get_permalink();
+
+                // if(get_post_meta(get_the_ID(), "qode_carousel-item-target", true) != ""){
+                //     $target = get_post_meta(get_the_ID(), "qode_carousel-item-target", true);
+                // } else {
+                //     $target = "_self";
+                // }
+                $target = "_self";
+
+                $title = get_the_title();
+
+                $post_layout = get_post_meta(get_the_ID(), "post_layout", true);
+
+                //is current item not on even position in array and two rows option is chosen?
+                if($postCount % 2 !== 0 && $show_in_two_rows == 'yes') {
+                    $html .= "<li class='item'>";
+                } elseif($show_in_two_rows == '') {
+                    $html .= "<li class='item'>";
+                }
+                $html .= '<div class="carousel_item_holder">';
+                if($link != ""){
+                    $html .= "<a href='".$link."' target='".$target."'>";
+                }
+
+                $first_image = qode_get_attachment_id_from_url($image);
+
+                if($image != ""){
+                    $html .= "<span class='first_image_holder ".$has_hover_image."'>";
+
+                    if(is_int($first_image)) {
+                        $html .= wp_get_attachment_image($first_image, 'full');
+                    } else {
+                        $html .= '<img src="'.$image.'" alt="carousel image" />';
+                        // $html .= $image;
+                    }
+
+
+                    $html .= "</span>";
+                }
+
+                $second_image = qode_get_attachment_id_from_url($hover_image);
+
+                if($hover_image != ""){
+                    $html .= "<span class='second_image_holder ".$has_hover_image."'>";
+
+                    // if(is_int($second_image)) {
+                    //     $html .= wp_get_attachment_image($second_image, 'full');
+                    // } else {
+                    //     $html .= '<img src="'.$hover_image.'" alt="carousel image" />';
+                    // }
+                    $html .= $title;
+
+
+                    $html .= "</span>";
+                }
+
+                if($link != ""){
+                    $html .= "</a>";
+                }
+
+                $html .= '</div>';
+
+                //is current item on even position in array and two rows option is chosen?
+                if($postCount % 2 == 0 && $show_in_two_rows == 'yes') {
+                    $html .= "</li>";
+                } elseif($show_in_two_rows == '') {
+                    $html .= "</li>";
+                }
+
+                $postCount++;
+
+            endwhile;
+
+            else:
+                $html .= __('Sorry, no posts matched your criteria.','qode');
+            endif;
+
+            wp_reset_query();
+
+            $html .= "</ul>";
+            $html .= "</div></div>";
+
+        }
+
+        return $html;
+    }
+}
+add_shortcode('qode_carousel_tuff', 'qode_carousel_tuff');
+
+
